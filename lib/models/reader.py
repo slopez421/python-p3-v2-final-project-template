0ce41183c09998578492dcf94c75c1f94dc9d900
Shania Lopez <slopez@fandm.edu>
2023-10-28 09:14:51 -0400

    name and name setter added to validate user input

--- lib/models/reader.py
+++ lib/models/reader.py
@@ -1,2 +1,26 @@
 from models.__init__ import CURSOR, CONN
 
+class Reader: 
+    all = {}
+
+    def __init__(self, name, favorite_genre, favorite_book, id=None):
+
+        self.name = name
+        self.favorite_genre = favorite_genre
+        self.favorite_book = favorite_book
+    
+    def __repr__(self):
+        return f"<Reader {self.name}'s favorite book is {self.favorite_book}. Reader {self.name}'s favorite genre is {self.favorite_genre}."
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name):
+            self._name = name
+        else: 
+            raise ValueError("Please enter a non-empty string.")
+        
+   
