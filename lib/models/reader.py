6aa7ca5dba22e94bfcd64d30db8d582317b31493
Shania Lopez <slopez@fandm.edu>
2023-10-28 09:23:13 -0400

    classmethod drop table added

--- lib/models/reader.py
+++ lib/models/reader.py
@@ -58,3 +58,13 @@ class Reader:
         CURSOR.execute(sql)
         CONN.commit()
     
+    @classmethod
+    def drop_table(cls):
+        sql = """
+            DROP TABLE IF EXISTS readers
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+    
+    
+
