7df828f1ed1d5151822be301d18883d102714b91
Shania Lopez <slopez@fandm.edu>
2023-10-29 10:34:42 -0400

    adding seeding data for debug

--- lib/debug.py
+++ lib/debug.py
@@ -8,6 +8,10 @@ import ipdb
 Reader.drop_table()
 Reader.create_table()
 
+alyssa = Reader.create("Alyssa", "Horror", "Harry Potter")
+yax = Reader.create("Yaxenis", "Fantasy", "Game of Thrones")
+shania = Reader.create("Shania", "Young Adult Fiction", "The Cruel Prince")
+
 
 
 ipdb.set_trace()
